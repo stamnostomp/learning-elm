37e2e341d0e90036351dc478ea6efc979af33db4
= <stamno@pm.me>
2024-09-09 14:16:31 -0600

    added playground file with some examples

---
+++ test-page/src/Playground.elm
@@ -0,0 +1,53 @@
+module Playground exposing (main)
+
+import Html
+
+
+escapeEarth myVelocity mySpeed fuleStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand fule =
+            if fule == "low" then
+                "Land on droneship"
+            else
+                "land on launchpad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "stay in orbit"
+    else
+        whereToLand fuleStatus
+
+computeSpeed distance time =
+    distance / time
+
+computeTime startTime endTime =
+    endTime - startTime
+
+add a b =
+    a + b
+
+multiply c d =
+    c * d
+
+divide e f =
+    e / f
+
+main =
+    escapeEarth 10 6.7 "low"
+        |> Html.text
+
+
+ --Html.text <| String.fromFloat <| add 5 <| multiply 10 <| divide 30 10
+  {-  computeTime 2 3
+        |> computeSpeed 7.67
+        |> escapeEarth 11
+        |> Html.text
+  -}
+    -- gross Html.text (escapeEarth 11 (computeSpeed 7.67 (computeTime 2 3))) -- need parentheces to make clear that we are only passing one argument to Html.text
